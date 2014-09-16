82a43989015d133acbe97615afef5d4b78aaa8cf
Daniel Rakhamimov <danielrak@me.com>
2014-09-16 14:37:12 -0400

    Delete Stock VC, add Feed VC

---
+++ Swift HN/FeedViewController.swift
@@ -0,0 +1,55 @@
+//
+//  FeedViewController.swift
+//  Swift HN
+//
+//  Created by Daniel on 9/16/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //MARK: 
+    //MARK: <UITableViewDataSource>
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+        
+    }
+    
+    //MARK:
+    //MARK: <UITableViewDelegate>
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
